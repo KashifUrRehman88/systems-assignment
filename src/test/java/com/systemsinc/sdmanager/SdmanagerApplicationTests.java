c6060a02b7f14be3fb30a3bce986c08aa1f22c1c
kaur <kaur@ciklum.com>
2023-08-25 10:16:52 +0500

    Create a new spring boot project by using spring initializr.

---
+++ src/test/java/com/systemsinc/sdmanager/SdmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemsinc.sdmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
