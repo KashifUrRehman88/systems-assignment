0d226f7ac0f9e77f397b0494f26b487ab07752ab
kaur <kaur@ciklum.com>
2023-08-25 10:34:51 +0500

    commit for testing

--- src/main/java/com/systemsinc/sdmanager/SdmanagerApplication.java
+++ src/main/java/com/systemsinc/sdmanager/SdmanagerApplication.java
@@ -3,6 +3,9 @@ package com.systemsinc.sdmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+commit for testing
+ */
 @SpringBootApplication
 public class SdmanagerApplication {
 
